921db05d9a09c4235bc537801868c31a4063afc3
Radziiwiil <radziiwiil@gmail.com>
2023-10-09 02:01:09 +0200

    modified:   RP_5D5/kalen/index.html 	modified:   RP_5D5/kalen/skrypt.js 	modified:   RP_5D5/kalen/style.css

--- RP_5D5/kalen/skrypt.js
+++ RP_5D5/kalen/skrypt.js
@@ -1,4 +1,69 @@
 window.onload = function() {
+    pierwsza();
+    druga();
+}
+
+
+function dni (mies, rok){
+    return new Date(rok, mies, 0).getDate();
+}
+
+
+function pierwsza() {
+    var data2 = new Date;
+    
+    for(let y=-1; y < 2; y++) {
+        op=0;
+        i=1;
+        a = y+2
+        var liczbad = dni(data2.getMonth()+1+y,data2.getFullYear());
+        var liczbadpop = dni(data2.getMonth()+y,data2.getFullYear());
+        var data3 = new Date(data2.getFullYear(),data2.getMonth()+y,1);
+        var pier = data3.getDay();
+        var dzien = data2.getDate();
+        pier = pier != 0 ? pier : 7;
+        z=-1*((liczbadpop-pier)+2);
+
+        while(z != liczbad) {
+            var tab = document.getElementById("tab"+ a);
+            var wiersz = tab.insertRow();
+
+            for (let x=0; x < 7; x++) {
+                z= Math.abs(z);
+                var rekord = wiersz.insertCell();
+                rekord.innerHTML = z;
+                z++;
+                i++;
+
+                if (op != 2 || (z < 7 && op == 1)) {
+                    rekord.style.backgroundColor = "rgb(158, 76, 0)";
+                }
+
+                if (op == 2 && y == 0 && z == dzien+1) {
+                    rekord.style.backgroundColor = "rgb(247, 202, 0)";
+                }
+
+                if (i == pier && op != 2) {
+                    z=1;
+                    op=2;
+                }
+
+                if (z == liczbad+1 && op == 2) {
+                    z=1; 
+                    op=1;         
+                }
+            }
+
+            if (op == 1) {
+                z = liczbad;                
+            }
+        }
+    }
+}
+
+
+
+function druga() {
     var data = new Date;
     var rok = data.getFullYear();
     var m1 = data.getMonth() - 1;
